ac9642d29046776c91ec9163ca071b205dfe4570
Abdul Domado <abdulm.domado@exist.com>
2023-12-10 20:18:17 +0800

    Update streamlit app and dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,43 @@
 import pandas as pd
 import streamlit as st
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics.pairwise import cosine_similarity
 
-recipe_df = pd.read_csv('recipe_with_ingredients_tfidf.csv')
+recipe_df = pd.read_csv('recipe_df_clustered.csv')
 options = recipe_df['recipe_name'].to_list()
-# Initialize the TfidfVectorizer
-tfidf_vectorizer = TfidfVectorizer()
+feature_cols = ['Calories(kcal)', 'Carbohydrates(g)', 'Protein(g)',
+               'Fat(g)', 'Saturated Fat(g)', 'Polyunsaturated Fat(g)', 'Sodium(mg)', 'Potassium(mg)',
+               'Fiber(g)', 'Sugar(g)', 'Vitamin A(IU)', 'Vitamin C(mg)', 'Calcium(mg)', 'Iron(mg)','Cholesterol(mg)']
 
-# Apply the vectorizer to the stringified token lists
-tfidf_matrix = tfidf_vectorizer.fit_transform(recipe_df['cleaned_tokenized_ingredients'])
 
-# Initialize NearestNeighbors
-nn = NearestNeighbors(metric='cosine', algorithm='brute')
-nn.fit(tfidf_matrix)
-
-st.title("Recipe Recommender")
-st.caption("Description of the app here")
+st.title("Recipe Recommende Engine")
 st.image("https://img.freepik.com/free-vector/tiny-female-chef-cooking-vegan-meal-using-recipe-kitchen-cook-making-dish-from-restaurant-menu-flat-vector-illustration-healthy-food-diet-culinary-nutrition-concept-website-design_74855-22063.jpg?w=1800&t=st=1701863073~exp=1701863673~hmac=694591b1fe66e00ba5db832192d8a7a8b4d3f8a3ed3f727bd9cad32131006599")
 st.divider()
 
 selectedRecipe = st.selectbox("Choose recipe:", options=options)
 
-def recommend(dish_name, num_recommendations=5):
-    dish_idx = recipe_df.index[recipe_df['recipe_name'] == dish_name].tolist()[0]
-    dish_tfidf_vector = tfidf_matrix[dish_idx]
-    neighbors = nn.kneighbors(dish_tfidf_vector, n_neighbors=num_recommendations + 1)
-    neighbor_indices = neighbors[1][0]
-    recommended_dishes = recipe_df['recipe_name'].iloc[neighbor_indices[1:]].tolist()
-    return recommended_dishes[:num_recommendations]
+def showNutrientInfo(seed_data):
+    st.markdown("<h5>" + seed_data['recipe_name'] + "</h5>", unsafe_allow_html = True)
+    st.text("Source: " + seed_data['source'])
+    st.table(data=seed_data[feature_cols])
+
+def get_cosine_dist(x,y):
+    cosine_dist = 1 - cosine_similarity(x.values.reshape(1, -1), y.values.reshape(1, -1)).flatten()[0]
+    return cosine_dist
 
 if st.button("Submit", type="primary"):
-	output = recommend(selectedRecipe)
-	st.divider()
-	st.markdown("<h5>Suggested recipes: </h5>", unsafe_allow_html = True)
-	for val in output:
-		st.info(val)
+    seed_data = recipe_df[recipe_df['recipe_name'] == selectedRecipe].iloc[0]
+    recipe_df['cosine_similarity_features'] = recipe_df.apply(lambda x: get_cosine_dist(x[feature_cols],seed_data[feature_cols]), axis=1)
+    output = recipe_df[(recipe_df['recipe_name']!=seed_data['recipe_name']) & (recipe_df['Cluster']==seed_data['Cluster'])].sort_values('cosine_similarity_features').reset_index(drop=True)
+    st.divider()
+    showNutrientInfo(seed_data)
 
+    max_euclidean = recipe_df['cosine_similarity_features'].max() 
+    min_euclidean = recipe_df['cosine_similarity_features'].min()
+    output['cosine_similarity_features_scaled'] =  (1 - output['cosine_similarity_features'] / (max_euclidean - min_euclidean)).abs()
+
+    st.markdown("<h5>Suggested recipes: </h5>", unsafe_allow_html = True)
+    for i, recipe in output[:5].iterrows():
+        #st.info(recipe['recipe_name'])
+        print(str(recipe['cosine_similarity_features']) + "-" + str(recipe['cosine_similarity_features_scaled']))
+        st.progress(value = recipe['cosine_similarity_features_scaled'])
+        showNutrientInfo(recipe)
